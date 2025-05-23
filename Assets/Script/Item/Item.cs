6c3a86253ee6c55f0fe232751e0e5d83835dc890
iristopvt <iristopvt@naver.com>
2025-05-23 15:52:25 +0900

    Refactoring

--- Assets/Script/Item/Item.cs
+++ Assets/Script/Item/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
 public class Item : ScriptableObject
 {
     public string itemName;            
